16e3b2217fe757f6f8b8bd963d5f9d4aebe62f75
Nathan Esquenazi <nesquena@gmail.com>
2017-02-04 16:33:58 -0800

    Update challenge_5.java

--- week_3/challenge_5.java
+++ week_3/challenge_5.java
@@ -1,2 +1,39 @@
 // Path Sums with DFS
-// No solution yet (contribute one?)
+
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+	public int hasPathSum(TreeNode A, int B) {
+
+	    boolean status = sum(A, 0, B);
+
+	    return status ? 1 : 0;
+	}
+
+	public boolean sum(TreeNode A, int curSum, int reqSum) {
+
+	    if (A == null) {
+	        return false;
+	    }
+
+	    if (A.left == null && A.right == null) {
+	        curSum += A.val;
+	        if (curSum == reqSum)
+	            return true;
+	        return false;
+	    }
+
+	    int sum = curSum + A.val;
+
+	    return sum(A.left, sum, reqSum) || sum(A.right, sum, reqSum);
+
+	}
+
+}
