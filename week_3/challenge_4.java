122c1fe9ede7cae2561ac63f2021c6f3c684f130
Franklin Ho <franklinho@users.noreply.github.com>
2017-02-05 13:41:12 -0800

    Solution for path sum
    Updated solution for path sum problem.

--- week_3/challenge_4.java
+++ week_3/challenge_4.java
@@ -1,4 +1,24 @@
 // Path Sum with BFS
 // https://www.interviewbit.com/problems/path-sum/
-// No solution yet (contribute one?)
 
+public class Solution {
+	public boolean hasPathSum(TreeNode a, int b) {
+	    if (a == null) {
+	        return b == 0;
+	    }
+	    
+	    if (a.left == null && a.right == null) {
+	        return b - a.val == 0 ? 1 : 0;
+	    }
+	    
+	    if (a.left == null) {
+	        return  hasPathSum(a.right, b - a.val) == 1 ;
+	    } else if (a.right == null) {
+	         return hasPathSum(a.left, b - a.val) == 1 ;
+	    } else {
+	        return hasPathSum(a.left, b - a.val) == 1 || hasPathSum(a.right, b-a.val) == 1 ;
+	    }
+	   
+	}
+	
+}
