f6974803f45b740d82144c28cdeabbb97fbe4e46
Bianca Curutan <bmcurutan@gmail.com>
2017-03-02 23:19:43 -0800

    Renamed challenge file (week 7, challenge 2) - Was incorrectly labeled challenge 1

--- week_7/challenge_2.java
+++ week_7/challenge_2.java
@@ -1,3 +1,14 @@
+/*
+Challenge 2 - Majority Element
+Majority Element Given an array of size n, find the majority element. The majority element is the element that appears more than floor(n/2) times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example :
+
+Input : [2, 1, 2] Return : 2 which occurs 2 times which is greater than 3/2.
+ */
+
 public class Solution {
     public int majorityElement(final List<Integer> a) {
         if (a == null || a.size() == 0){
