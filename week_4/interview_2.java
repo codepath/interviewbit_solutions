bbbb5c2ebf10c400b3ebb66c417971ba664ceef2
Sahil Agarwal <sahil.agarwal@xoom.com>
2017-08-03 15:43:45 -0700

    Week 4: Interview 2 (Calculating n choose k)

--- week_4/interview_2.java
+++ week_4/interview_2.java
@@ -1,2 +1,45 @@
 // Interview Question 2: Calculating n choose k
-// Please contribute your solution to this repo by submitting a PR!
+
+import java.util.HashMap;
+
+public class interview_2 {
+	
+	public static long nChooseKRecursive(int n, int k) {
+		if (k >= n) {
+			return 1L;
+		} else if (k == 1) {
+			return (long) n;
+		}
+		return nChooseKRecursive(n - 1, k - 1) + nChooseKRecursive(n - 1, k);
+	}
+
+	public static long nChooseKRecursiveWithMemoization(int n, int k) {
+		HashMap<String, Long> map = new HashMap<>();		
+		return nChooseKRecursiveWithMemoizationHelper(n, k, map);
+	}
+
+	private static long nChooseKRecursiveWithMemoizationHelper(int n, int k, HashMap<String, Long> map) {
+		if (k >= n) {			
+			return 1L;
+		} else if (k == 1) {
+			return (long) n;
+		}
+
+		String key = n + "" + k;
+		if (map.containsKey(key)) {
+			return map.get(key);
+		}
+		long sum = nChooseKRecursive(n - 1, k - 1) + nChooseKRecursive(n - 1, k);
+		map.put(key, sum);
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		long sum2 = nChooseKRecursiveWithMemoization(20, 4);  // 4845
+		System.out.println(sum2);
+
+		long sum = nChooseKRecursive(20, 4);  // 4845
+		System.out.println(sum);
+	}
+
+}
